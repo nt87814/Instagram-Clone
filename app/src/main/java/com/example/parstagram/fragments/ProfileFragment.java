82d904420a1b7efe288e225908a227bda270d023
Natasha Trayers <ntrayers@fb.com>
2020-07-08 16:32:07 -0700

    User can log out of account

--- app/src/main/java/com/example/parstagram/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/parstagram/fragments/ProfileFragment.java
@@ -1,16 +1,60 @@
 package com.example.parstagram.fragments;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.parstagram.LoginActivity;
+import com.example.parstagram.MainActivity;
 import com.example.parstagram.Post;
+import com.example.parstagram.PostsAdapter;
+import com.example.parstagram.R;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ProfileFragment extends PostsFragment{
+
+//    RecyclerView rvProfilePosts;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_profile, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.btnLogout) {
+            ParseUser.logOut();
+            goLoginActivity();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void queryPosts() {
         ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
@@ -34,5 +78,30 @@ public class ProfileFragment extends PostsFragment{
                 adapter.notifyDataSetChanged();
             }
         });
+    }
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        return inflater.inflate(R.layout.fragment_profile, container, false);
+//    }
+//
+//    @Override
+//    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+////        super.onViewCreated(view, savedInstanceState);
+//        rvProfilePosts = view.findViewById(R.id.rvProfilePosts);
+//
+//        allPosts = new ArrayList<>();
+//        adapter = new PostsAdapter(getContext(), allPosts);
+//        rvProfilePosts.setAdapter(adapter);
+//        rvProfilePosts.setLayoutManager(new LinearLayoutManager(getContext()));
+//        queryPosts();
+//    }
+
+    private void goLoginActivity() {
+        Intent i = new Intent(getActivity(), LoginActivity.class);
+        startActivity(i);
+        getActivity().finish();
     }
 }
